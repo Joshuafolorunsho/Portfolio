ada7b4ee93f55710f33bf68f25535317f975fa45
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-03-30 15:54:06 +0100

    toggle logic bug fixed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 const hamburger = document.querySelector('.hamburger');
+var bigNav = document.querySelector('.big-nav');
 const header = document.querySelector('.header');
 
 
 
 const toggle = () => {
    header.classList.toggle('toggle');
+
+   if (bigNav.style.display === 'none') {
+      bigNav.style.display = 'block';
+   } else {
+      bigNav.style.display = 'none';
+   }
 } 
 
 hamburger.addEventListener('click', toggle);
