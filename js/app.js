d8d7593f3cc1c90b37e862d11e44fb54d8343835
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-04-02 11:35:43 +0100

    refactor js code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
-const hamburger = document.querySelector('.hamburger');
-var bigNav = document.querySelector('.big-nav');
-const header = document.querySelector('.header');
+const ham =(function() {
+   const hamburger = document.querySelector('.hamburger');
+   var bigNav = document.querySelector('.big-nav');
+   const header = document.querySelector('.header');
 
 
+   const toggle = () => {
+      header.classList.toggle('toggle');
 
-const toggle = () => {
-   header.classList.toggle('toggle');
+      (bigNav.style.display === 'none') ?
+         bigNav.style.display = 'block' :
+         bigNav.style.display = 'none';
+      
+   } 
 
-   (bigNav.style.display === 'none') ?
-      bigNav.style.display = 'block' :
-      bigNav.style.display = 'none';
-   
-} 
-
-hamburger.addEventListener('click', toggle);
+   hamburger.addEventListener('click', toggle);
+})()
