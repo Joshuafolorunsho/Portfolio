0c3de8a21216c22ad017b0cbd156b237ae39ac0a
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-04-02 11:40:21 +0100

    implemented scroll effect

--- js/app.js
+++ js/app.js
@@ -14,4 +14,29 @@ const ham =(function() {
    } 
 
    hamburger.addEventListener('click', toggle);
-})()
+})();
+
+const scrollEffect = (function() {
+   let mainNavLinks = document.querySelectorAll("nav ul li a");
+   let mainSections = document.querySelectorAll("main section");
+
+   let lastId;
+   let cur = [];
+
+   window.addEventListener("scroll", event => {
+      let fromTop = window.scrollY;
+
+      mainNavLinks.forEach(link => {
+         let section = document.querySelector(link.hash);
+
+         if (
+            section.offsetTop <= fromTop &&
+            section.offsetTop + section.offsetHeight > fromTop
+         ) {
+            link.classList.add("current");
+         } else {
+            link.classList.remove("current");
+         }
+      });
+   });
+})();
